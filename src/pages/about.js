e7f9b798beeb3e7ff8e657e817b6d4dd4ed749e1
Vanjoo667 <joelajideademola@gmail.com>
2021-07-10 01:52:00 +0100

    added 2 test pages and some stylings

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const About = () => {
+    return (
+        <Layout pageTitle="About Page">
+            <p>I'm making this by following the Gatsby Tutorial.</p>
+        </Layout>
+    )
+}
+
+export default About
